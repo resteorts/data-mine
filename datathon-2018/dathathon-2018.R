7dbb3916c4ece2566858f5bfb386aa4222e642fd
Rebecca Steorts <beka@stat.duke.edu>
2018-10-18 10:19:55 -0400

    updated datathon.

---
+++ datathon-2018/dathathon-2018.R
@@ -0,0 +1,21 @@
+library(plyr)
+
+underName <- c("Agarwal, Naman", "Alexander, Ben", "Chen, Carolyn", "Deng, Ziqi", "Donahoe, Grace", " Donheiser, Julia", "Erfurth, Sophie", "Gao,An", "Giraud, Paul", "Gu, Yunxiang", "Holland, Ethan", "Hsiao, Man-Lin", "Huang, Howard", "Jiang, Debra", "Lin, Vincent", "Lyu, Cheng", "Ma, Calvin", "Malone, Alexis", "Marsalek, Oskar", "Metz, Audreya", "Rajan, Hari", "San-Martin, Jose", "Shmueli, Sara", "Tan, Michael", "Taylor, Moriah", "Wang, Harry", "Wohl, Julia", "Xiao, Claire", "Xu,Yingzhi")
+
+# 29 students in the class
+length(underName)
+
+groupLeaders <- c("Donheiser, Julia ", "Wohl, Julia", "Rajan, Hari (Starke)", "Donahoe, Grace", "Chen, Carolyn", "Holland, Ethan", "Ben Alexander")
+Donheiser <- c("Agarwal, Naman", "Deng, Ziqi","Lin, Vincent")
+Wohl <- c("Erfurth, Sophie", "Gao,An", "Lyu, Cheng", "Xu,Yingzhi")
+Rajan <- c("Metz, Audreya", "Giraud, Paul", "Ma, Calvin")
+Donahoe <- c("Marsalek, Oskar", "Gu, Yunxiang","Shmueli, Sara")
+Chen <- c("San-Martin, Jose", "Hsiao, Man-Lin","Tan, Michael")
+Holland <- c("Taylor, Moriah", "Huang, Howard","Wang, Harry")
+Alexander <- c("Malone, Alexis", "Jiang, Debra","Xiao, Claire")
+
+
+
+
+
+
