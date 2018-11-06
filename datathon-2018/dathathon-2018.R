977af738acb47dc1d4eeaeae3871686041445556
Rebecca Steorts <beka@stat.duke.edu>
2018-11-06 10:35:17 -0500

    updated template.

--- datathon-2018/dathathon-2018.R
+++ datathon-2018/dathathon-2018.R
@@ -5,7 +5,7 @@ underName <- c("Agarwal, Naman", "Alexander, Ben", "Chen, Carolyn", "Deng, Ziqi"
 # 29 students in the class
 length(underName)
 
-groupLeaders <- c("Donheiser, Julia ", "Wohl, Julia", "Rajan, Hari (Starke)", "Donahoe, Grace", "Chen, Carolyn", "Holland, Ethan", "Ben Alexander")
+groupLeaders <- c("Donheiser, Julia ", "Wohl, Julia", "Rajan, Hari", "Donahoe, Grace", "Chen, Carolyn", "Holland, Ethan", "Ben Alexander")
 Donheiser <- c("Agarwal, Naman", "Deng, Ziqi","Lin, Vincent")
 Wohl <- c("Gao,An", "Lyu, Cheng", "Xu,Yingzhi")
 Rajan <- c("Metz, Audreya", "Giraud, Paul", "Ma, Calvin")
@@ -14,7 +14,22 @@ Chen <- c("San-Martin, Jose", "Hsiao, Man-Lin","Tan, Michael")
 Holland <- c("Taylor, Moriah", "Huang, Howard","Wang, Harry")
 Alexander <- c("Malone, Alexis", "Jiang, Debra","Xiao, Claire")
 
-github_handles <- c(??, jbw47, harirajan0, gracedonahoe, resteorts, EthanJHolland, ba305)
+github_handles <- c(juliarenee, jbw47, harirajan0, gracedonahoe, resteorts, EthanJHolland, ba305)
+
+github_repos <- c(
+https://github.com/juliarenee/datathon18-.Rmasters/tree/master/Homework%207
+https://github.com/jbw47/stat-sleuths.git
+https://github.com/harirajan0/NormalCurvisaurus/tree/master/HW7_final
+https://github.com/gracedonahoe/DonahoeTeam
+https://github.com/resteorts/HashtagAwesome/tree/master/hw-pca
+https://github.com/EthanJHolland/ParanormalDistributionDatathon/tree/master/hw7
+https://github.com/ba305/Datathon-Team-ABCD)
+
+team_numbers <- c(69, 24, 99, 77, 10, 100, 21)
+
+
+
+
 
 
 
